c72ea3f468ba24dd20b175ec9cf818e16584bfa2
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-19 08:38:40 +0300

    Created authentication project

--- authentication/wsgi.py
+++ authentication/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for user_authentication project.
+WSGI config for authentication project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_authentication.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authentication.settings')
 
 application = get_wsgi_application()
