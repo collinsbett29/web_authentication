f243117b261f13009046efee5ef8dde7ab614d4f
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-19 13:40:51 +0300

    Make migrations

--- web_authentication/user_authentication/settings.py
+++ web_authentication/user_authentication/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'user_authentication.urls'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 
 TEMPLATES = [
     {
