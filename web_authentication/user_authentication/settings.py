69b2bc57eb022a6175aaec6c8880c0270e1dac88
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-22 23:32:09 +0300

    Deployment

--- web_authentication/user_authentication/settings.py
+++ web_authentication/user_authentication/settings.py
@@ -93,8 +93,8 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
+           'NAME': config('authentication'),
+           'USER': config('Collins'),
            'PASSWORD': config('##@@collins1'),
            'HOST': config('127.0.0.1'),
            'PORT': '5433',
